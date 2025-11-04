4bb52173b33454f0405cddd2138e354bacefb5f6
ruddmslee <dlruddms326@naver.com>
2025-11-04 10:41:41 +0900

    ✨ UserStockHoldingRepository에 findByUserAndStock 추가

--- src/main/java/com/hackathon/tomolow/domain/userStockHolding/repository/UserStockHoldingRepository.java
+++ src/main/java/com/hackathon/tomolow/domain/userStockHolding/repository/UserStockHoldingRepository.java
@@ -1,7 +1,13 @@
 package com.hackathon.tomolow.domain.userStockHolding.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.hackathon.tomolow.domain.stock.entity.Stock;
+import com.hackathon.tomolow.domain.user.entity.User;
 import com.hackathon.tomolow.domain.userStockHolding.entity.UserStockHolding;
 
-public interface UserStockHoldingRepository extends JpaRepository<UserStockHolding, Long> {}
+public interface UserStockHoldingRepository extends JpaRepository<UserStockHolding, Long> {
+  Optional<UserStockHolding> findByUserAndStock(User user, Stock stock);
+}
