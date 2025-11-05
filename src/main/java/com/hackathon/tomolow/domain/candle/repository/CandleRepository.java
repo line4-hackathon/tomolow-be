1f32428a3df5a22be694c3c3bbe2f28c0c563a9f
hamtorygoals <yhaemin0531@naver.com>
2025-11-05 12:25:40 +0900

    :recycle: 종목 관련 entity 수정

--- src/main/java/com/hackathon/tomolow/domain/candle/repository/CandleRepository.java
+++ src/main/java/com/hackathon/tomolow/domain/candle/repository/CandleRepository.java
@@ -1,8 +1,7 @@
 package com.hackathon.tomolow.domain.candle.repository;
 
-import com.hackathon.tomolow.domain.candle.entity.Candle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CandleRepository extends JpaRepository<Candle, Long> {
+import com.hackathon.tomolow.domain.candle.entity.Candle;
 
-}
+public interface CandleRepository extends JpaRepository<Candle, Long> {}
--- src/main/java/com/hackathon/tomolow/domain/stock/entity/Stock.java
+++ src/main/java/com/hackathon/tomolow/domain/stock/entity/Stock.java
@@ -1,6 +1,5 @@
 package com.hackathon.tomolow.domain.stock.entity;
 
-import com.hackathon.tomolow.global.common.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -9,6 +8,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+
+import com.hackathon.tomolow.global.common.BaseTimeEntity;
+
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
