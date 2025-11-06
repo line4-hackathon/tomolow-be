3bceab918efb00894b58eb03832bbe928371f21c
hamtorygoals <yhaemin0531@naver.com>
2025-11-06 18:45:52 +0900

    :wrench: scheduler 기능 가능하도록 어노테이션 추가

--- src/main/java/com/hackathon/tomolow/TomolowBeApplication.java
+++ src/main/java/com/hackathon/tomolow/TomolowBeApplication.java
@@ -3,9 +3,11 @@ package com.hackathon.tomolow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing // Spring Data JPA에서, Auditing(감시)기능을 활성화하는 어노테이션
+@EnableScheduling
 public class TomolowBeApplication {
 
   public static void main(String[] args) {
