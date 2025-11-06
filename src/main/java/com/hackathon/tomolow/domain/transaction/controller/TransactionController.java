1155abf42087b6a93d7980099122a2fed381afa1
ruddmslee <dlruddms326@naver.com>
2025-11-07 02:59:54 +0900

    ✨ 시장가 매수/매도 Controller 추가

--- src/main/java/com/hackathon/tomolow/domain/transaction/controller/TransactionController.java
+++ src/main/java/com/hackathon/tomolow/domain/transaction/controller/TransactionController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.hackathon.tomolow.domain.transaction.dto.OrderRequestDto;
 import com.hackathon.tomolow.domain.transaction.service.LimitTransactionService;
+import com.hackathon.tomolow.domain.transaction.service.MarketTransactionService;
 import com.hackathon.tomolow.global.response.BaseResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -24,9 +25,10 @@ import lombok.RequiredArgsConstructor;
 public class TransactionController {
 
   private final LimitTransactionService limitTransactionService;
+  private final MarketTransactionService marketTransactionService;
 
   @PostMapping("/buy/limit/{marketId}")
-  @Operation(summary = "지정가 매도", description = "지정가 매도를 위한 API")
+  @Operation(summary = "지정가 매수", description = "지정가 매수를 위한 API")
   public ResponseEntity<BaseResponse<?>> limitBuyOrder(
       @PathVariable Long marketId, @Valid @RequestBody OrderRequestDto orderRequestDto) {
     String buyOrderId = limitTransactionService.limitBuy(marketId, orderRequestDto);
@@ -34,10 +36,26 @@ public class TransactionController {
   }
 
   @PostMapping("/sell/limit/{marketId}")
-  @Operation(summary = "지정가 매수", description = "지정가 매수를 위한 API")
+  @Operation(summary = "지정가 매도", description = "지정가 매도를 위한 API")
   public ResponseEntity<BaseResponse<?>> limitSellOrder(
       @PathVariable Long marketId, @Valid @RequestBody OrderRequestDto orderRequestDto) {
     String sellOrderId = limitTransactionService.limitSell(marketId, orderRequestDto);
     return ResponseEntity.ok(BaseResponse.success(sellOrderId));
+  }
+
+  @PostMapping("/buy/market/{marketId}")
+  @Operation(summary = "시장가 매수", description = "시장가 매수를 위한 API")
+  public ResponseEntity<BaseResponse<?>> marketBuyOrder(
+      @PathVariable Long marketId, @Valid @RequestBody OrderRequestDto orderRequestDto) {
+    marketTransactionService.marketBuy(marketId, orderRequestDto);
+    return ResponseEntity.ok(BaseResponse.success(null));
+  }
+
+  @PostMapping("/sell/market/{marketId}")
+  @Operation(summary = "시장가 매도", description = "시장가 매도를 위한 API")
+  public ResponseEntity<BaseResponse<?>> marketSellOrder(
+      @PathVariable Long marketId, @Valid @RequestBody OrderRequestDto orderRequestDto) {
+    marketTransactionService.marketSell(marketId, orderRequestDto);
+    return ResponseEntity.ok(BaseResponse.success(null));
   }
 }
