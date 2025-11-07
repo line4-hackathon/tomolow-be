dc5ec1bbb98539541a9ea38a6c04e75ef660d8b2
ruddmslee <dlruddms326@naver.com>
2025-11-08 04:43:01 +0900

    ✨ 채팅방 불러오기 관련 ChatErrorCode 추가

--- src/main/java/com/hackathon/tomolow/domain/chat/exception/ChatErrorCode.java
+++ src/main/java/com/hackathon/tomolow/domain/chat/exception/ChatErrorCode.java
@@ -11,7 +11,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ChatErrorCode implements BaseErrorCode {
   EXTERNAL_API_ERROR(
-      "CHAT_4001", "외부 API와 연결에 실패했거나 응답이 유효하지 않습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+      "CHAT_4001", "외부 API와 연결에 실패했거나 응답이 유효하지 않습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
+  JSON_MAPPING_ERROR("CHAT_4002", "채팅 JSON 직렬화 과정에서 문제가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
+  ;
 
   private final String code;
   private final String message;
