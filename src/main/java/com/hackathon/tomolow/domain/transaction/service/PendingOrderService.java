5f5069c4b09e8a38b12339eaf3dbaed529258918
ruddmslee <dlruddms326@naver.com>
2025-11-09 06:41:33 +0900

    ✨ 시장가 조회, 대기주문 수정 기능 구현

--- src/main/java/com/hackathon/tomolow/domain/transaction/service/PendingOrderService.java
+++ src/main/java/com/hackathon/tomolow/domain/transaction/service/PendingOrderService.java
@@ -2,16 +2,18 @@ package com.hackathon.tomolow.domain.transaction.service;
 
 import java.math.BigDecimal;
 
-import com.hackathon.tomolow.domain.transaction.dto.PendingOrderModifyRequestDto;
-import com.hackathon.tomolow.domain.transaction.entity.TradeType;
-import com.hackathon.tomolow.domain.user.exception.UserErrorCode;
-import com.hackathon.tomolow.domain.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import com.hackathon.tomolow.domain.market.entity.Market;
 import com.hackathon.tomolow.domain.market.exception.MarketErrorCode;
 import com.hackathon.tomolow.domain.market.repository.MarketRepository;
 import com.hackathon.tomolow.domain.ticker.service.PriceQueryService;
+import com.hackathon.tomolow.domain.transaction.dto.PendingOrderModifyRequestDto;
+import com.hackathon.tomolow.domain.transaction.entity.TradeType;
+import com.hackathon.tomolow.domain.transaction.exception.TransactionErrorCode;
+import com.hackathon.tomolow.domain.user.entity.User;
+import com.hackathon.tomolow.domain.user.exception.UserErrorCode;
+import com.hackathon.tomolow.domain.user.repository.UserRepository;
 import com.hackathon.tomolow.global.exception.CustomException;
 
 import lombok.RequiredArgsConstructor;
@@ -26,34 +28,51 @@ public class PendingOrderService {
   private final UserRepository userRepository;
   private final MatchService matchService;
 
-  public BigDecimal getLatestMarketPrice(Long marketId) {
+  public BigDecimal getLatestMarketPrice(String orderId) {
+    String marketId = orderRedisService.getOrderMarketId(orderId);
     Market market =
         marketRepository
-            .findById(marketId)
+            .findById(Long.valueOf(marketId))
             .orElseThrow(() -> new CustomException(MarketErrorCode.MARKET_NOT_FOUND));
     return priceQueryService.getLastTradePriceOrThrow(market.getSymbol());
   }
 
-  public void modifyPendingOrder(Long userId, Long marketId, PendingOrderModifyRequestDto modifyRequestDto) {
-    Market market = marketRepository
-            .findById(marketId)
-            .orElseThrow(() -> new CustomException(MarketErrorCode.MARKET_NOT_FOUND));
-
-    userRepository.findById(userId).orElseThrow(
-            () -> new CustomException(UserErrorCode.USER_NOT_FOUND));
+  public void modifyPendingOrder(Long userId, PendingOrderModifyRequestDto modifyRequestDto) {
 
     String orderId = modifyRequestDto.getOrderId();
     BigDecimal price = modifyRequestDto.getPrice();
-    TradeType tradeType = modifyRequestDto.getTradeType();
+
+    String marketId = orderRedisService.getOrderMarketId(orderId);
+    Market market =
+        marketRepository
+            .findById(Long.valueOf(marketId))
+            .orElseThrow(() -> new CustomException(MarketErrorCode.MARKET_NOT_FOUND));
+
+    User user =
+        userRepository
+            .findById(userId)
+            .orElseThrow(() -> new CustomException(UserErrorCode.USER_NOT_FOUND));
+
+    TradeType tradeType = orderRedisService.getTradeType(orderId);
+    if (tradeType == null) throw new CustomException(TransactionErrorCode.TRADE_TYPE_NULL);
+
+    // 매수 주문 - ( 갱신할 가격 * 수량 ) > user의 현금 자산인 경우 수정 불가
+    if (tradeType == TradeType.BUY) {
+      int remainingQuantity = orderRedisService.getRemainingQuantity(orderId);
+      BigDecimal totalPrice = price.multiply(BigDecimal.valueOf(remainingQuantity));
+      if (totalPrice.compareTo(user.getCashBalance()) > 0)
+        throw new CustomException(
+            TransactionErrorCode.INSUFFICIENT_BALANCE, "잔액이 부족해서 수정할 수 없습니다.");
+    }
 
     // order book (ZSET) 갱신
-    orderRedisService.updateOrderBook(orderId, String.valueOf(marketId), tradeType, price);
+    orderRedisService.updateOrderBook(orderId, marketId, tradeType, price);
 
     // detail (HASH) 갱신
-    orderRedisService.updatePrice(modifyRequestDto.getOrderId(), modifyRequestDto.getPrice());
+    orderRedisService.updatePrice(orderId, price);
 
     // 매칭 시도
     BigDecimal marketPrice = priceQueryService.getLastTradePriceOrThrow(market.getSymbol());
-    matchService.matchByMarketPrice(String.valueOf(marketId), marketPrice);
+    matchService.matchByMarketPrice(marketId, marketPrice);
   }
 }
