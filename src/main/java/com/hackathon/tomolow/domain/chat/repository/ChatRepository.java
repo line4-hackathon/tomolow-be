3dcfe528beb1a41544b4f5e03b2e87c99a0173b3
ruddmslee <dlruddms326@naver.com>
2025-11-08 16:12:05 +0900

    ✨ 저장된 채팅 조회 기능 구현

--- src/main/java/com/hackathon/tomolow/domain/chat/repository/ChatRepository.java
+++ src/main/java/com/hackathon/tomolow/domain/chat/repository/ChatRepository.java
@@ -1,5 +1,8 @@
 package com.hackathon.tomolow.domain.chat.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hackathon.tomolow.domain.chat.entity.Chat;
@@ -7,4 +10,6 @@ import com.hackathon.tomolow.domain.user.entity.User;
 
 public interface ChatRepository extends JpaRepository<Chat, Long> {
   boolean existsByUserAndKey(User user, String key);
+
+  Optional<List<Chat>> findByUserId(Long userId);
 }
---
+++ src/main/java/com/hackathon/tomolow/domain/user/controller/UserChatController.java
@@ -0,0 +1,36 @@
+package com.hackathon.tomolow.domain.user.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hackathon.tomolow.domain.user.dto.response.UserChatResponseDto;
+import com.hackathon.tomolow.domain.user.service.UserChatService;
+import com.hackathon.tomolow.global.response.BaseResponse;
+import com.hackathon.tomolow.global.security.CustomUserDetails;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/mypage")
+@Tag(name = "User", description = "유저 관련 API")
+public class UserChatController {
+
+  private final UserChatService userChatService;
+
+  @GetMapping("/saved-chat")
+  public ResponseEntity<BaseResponse<?>> getSavedChats(
+      @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+    Long userId = customUserDetails.getUser().getId();
+
+    List<UserChatResponseDto> userChats = userChatService.getUserChats(userId);
+
+    return ResponseEntity.ok(BaseResponse.success(userChats));
+  }
+}
