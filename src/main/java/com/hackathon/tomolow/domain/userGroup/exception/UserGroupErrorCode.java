b2c58428a009fbc134cd83f527cebcc6928aa960
ruddmslee <dlruddms326@naver.com>
2025-11-11 00:26:24 +0900

    ✨ UserGroupErrorCode 추가

--- src/main/java/com/hackathon/tomolow/domain/userGroup/exception/UserGroupErrorCode.java
+++ src/main/java/com/hackathon/tomolow/domain/userGroup/exception/UserGroupErrorCode.java
@@ -10,7 +10,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum UserGroupErrorCode implements BaseErrorCode {
-  USER_GROUP_NOT_FOUND("USER_GROUP_4004", "그룹 내 사용자를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+  USER_GROUP_NOT_FOUND("USER_GROUP_4001", "그룹 내 사용자를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+  GROUP_NOT_ACTIVE_YET("USER_GROUP_4002", "그룹이 아직 모집 중 상태입니다.", HttpStatus.BAD_REQUEST);
 
   private final String code; // 에러 코드 문자열
   private final String message; // 에러 메시지
