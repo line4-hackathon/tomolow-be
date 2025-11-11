f12df28248d82663d9c9ea1df58357ee38e37c08
ruddmslee <dlruddms326@naver.com>
2025-11-12 04:50:45 +0900

    ✨ GroupScheduler에서 그룹 종료 시 finalPnL 저장 후 시드머니 몰아주기 구현

--- src/main/java/com/hackathon/tomolow/global/scheduler/GroupScheduler.java
+++ src/main/java/com/hackathon/tomolow/global/scheduler/GroupScheduler.java
@@ -1,5 +1,6 @@
 package com.hackathon.tomolow.global.scheduler;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -9,6 +10,14 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.hackathon.tomolow.domain.group.entity.Group;
 import com.hackathon.tomolow.domain.group.repository.GroupRepository;
+import com.hackathon.tomolow.domain.user.entity.User;
+import com.hackathon.tomolow.domain.user.exception.UserErrorCode;
+import com.hackathon.tomolow.domain.user.repository.UserRepository;
+import com.hackathon.tomolow.domain.userGroup.dto.UserGroupRankingDto;
+import com.hackathon.tomolow.domain.userGroup.entity.UserGroup;
+import com.hackathon.tomolow.domain.userGroup.service.UserGroupRankingService;
+import com.hackathon.tomolow.domain.userGroupTransaction.service.GroupOrderInfoService;
+import com.hackathon.tomolow.global.exception.CustomException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,6 +26,9 @@ import lombok.RequiredArgsConstructor;
 public class GroupScheduler {
 
   private final GroupRepository groupRepository;
+  private final UserGroupRankingService userGroupRankingService;
+  private final GroupOrderInfoService groupOrderInfoService;
+  private final UserRepository userRepository;
 
   @Transactional
   @Scheduled(fixedRate = 300000) // 5분마다
@@ -24,9 +36,26 @@ public class GroupScheduler {
     LocalDateTime now = LocalDateTime.now();
     List<Group> expiredGroups = groupRepository.findExpiredGroups(now);
     for (Group group : expiredGroups) {
+      Long winnerId = 0L;
+      // 최종 pnl 저장
+      List<UserGroupRankingDto> rankingAndPnLInGroup =
+          userGroupRankingService.getRankingAndPnLInGroup(group.getId());
+      for (UserGroupRankingDto rankingDto : rankingAndPnLInGroup) {
+        UserGroup userGroup =
+            groupOrderInfoService.getUserGroup(rankingDto.getUserPnl().getUserId(), group.getId());
+        userGroup.setGroupFinalPnL(rankingDto.getUserPnl().getPnL());
+        // 1위인 user의 id 저장
+        if (rankingDto.getRanking() == 1) winnerId = rankingDto.getUserPnl().getUserId();
+      }
       // 비활성화
       group.setGroupActive(false);
-      // TODO : 1등에게 나머지 팀원의 시드머니 몰아주기
+      // 1등에게 나머지 팀원의 시드머니 몰아주기
+      User winner =
+          userRepository
+              .findById(winnerId)
+              .orElseThrow(() -> new CustomException(UserErrorCode.USER_NOT_FOUND));
+      winner.addCashBalance(
+          group.getSeedMoney().multiply(BigDecimal.valueOf(group.getMemberCount())));
     }
   }
 }
