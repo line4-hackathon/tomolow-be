5dcbf24118bc1add74a94eab7839b1c4a0f72266
hamtorygoals <yhaemin0531@naver.com>
2025-11-12 13:13:50 +0900

    :memo: 스웨거 문서 수정, endpoint 수정

--- src/main/java/com/hackathon/tomolow/domain/userMarketHolding/controller/UserMarketHoldingController.java
+++ src/main/java/com/hackathon/tomolow/domain/userMarketHolding/controller/UserMarketHoldingController.java
@@ -1,18 +1,16 @@
 package com.hackathon.tomolow.domain.userMarketHolding.controller;
 
+import com.hackathon.tomolow.domain.userMarketHolding.dto.HoldingsResponse;
+import com.hackathon.tomolow.domain.userMarketHolding.service.HoldingQueryService;
+import com.hackathon.tomolow.global.response.BaseResponse;
+import com.hackathon.tomolow.global.security.CustomUserDetails;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.hackathon.tomolow.domain.userMarketHolding.dto.HoldingsResponse;
-import com.hackathon.tomolow.domain.userMarketHolding.service.HoldingQueryService;
-import com.hackathon.tomolow.global.response.BaseResponse;
-import com.hackathon.tomolow.global.security.CustomUserDetails;
-
-import io.swagger.v3.oas.annotations.Operation;
-import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,8 +19,8 @@ public class UserMarketHoldingController {
 
   private final HoldingQueryService holdingQueryService;
 
-  @GetMapping("/mypage/holdings/my")
-  @Operation(summary = "내 보유 종목 + 총 손익 조회(실시간 가격 반영)")
+  @GetMapping("/home/assets/my")
+  @Operation(summary = "홈화면 진입 시 현재 내 자산 조회 + 내 보유 종목 정보 + 총 손익 조회(실시간 가격 반영)")
   public ResponseEntity<BaseResponse<HoldingsResponse>> myHoldings(
       @AuthenticationPrincipal CustomUserDetails userDetails) {
     var body = holdingQueryService.getMyHoldings(userDetails.getUser());
