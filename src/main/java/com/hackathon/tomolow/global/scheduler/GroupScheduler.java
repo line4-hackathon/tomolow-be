7ffc370a0b7229bfbc0a2f4613481f9234b6dec9
ruddmslee <dlruddms326@naver.com>
2025-11-12 13:58:34 +0900

    ✨ 그룹 내 거래가 없을 경우 몰아주기 X 재분배하도록 추가

--- src/main/java/com/hackathon/tomolow/global/scheduler/GroupScheduler.java
+++ src/main/java/com/hackathon/tomolow/global/scheduler/GroupScheduler.java
@@ -10,14 +10,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.hackathon.tomolow.domain.group.entity.Group;
 import com.hackathon.tomolow.domain.group.repository.GroupRepository;
-import com.hackathon.tomolow.domain.user.entity.User;
-import com.hackathon.tomolow.domain.user.exception.UserErrorCode;
 import com.hackathon.tomolow.domain.user.repository.UserRepository;
 import com.hackathon.tomolow.domain.userGroup.dto.UserGroupRankingDto;
 import com.hackathon.tomolow.domain.userGroup.entity.UserGroup;
 import com.hackathon.tomolow.domain.userGroup.service.UserGroupRankingService;
+import com.hackathon.tomolow.domain.userGroupTransaction.repository.UserGroupTransactionRepository;
 import com.hackathon.tomolow.domain.userGroupTransaction.service.GroupOrderInfoService;
-import com.hackathon.tomolow.global.exception.CustomException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -29,6 +27,7 @@ public class GroupScheduler {
   private final UserGroupRankingService userGroupRankingService;
   private final GroupOrderInfoService groupOrderInfoService;
   private final UserRepository userRepository;
+  private final UserGroupTransactionRepository userGroupTransactionRepository;
 
   @Transactional
   @Scheduled(fixedRate = 300000) // 5분마다
@@ -37,7 +36,7 @@ public class GroupScheduler {
     List<Group> expiredGroups = groupRepository.findExpiredGroups(now);
     for (Group group : expiredGroups) {
       Long winnerId = 0L;
-      // 최종 pnl 저장
+      // 1. 최종 pnl 저장
       List<UserGroupRankingDto> rankingAndPnLInGroup =
           userGroupRankingService.getRankingAndPnLInGroup(group.getId());
       for (UserGroupRankingDto rankingDto : rankingAndPnLInGroup) {
@@ -47,15 +46,32 @@ public class GroupScheduler {
         // 1위인 user의 id 저장
         if (rankingDto.getRanking() == 1) winnerId = rankingDto.getUserPnl().getUserId();
       }
-      // 비활성화
+
+      // 2. 비활성화
       group.setGroupActive(false);
-      // 1등에게 나머지 팀원의 시드머니 몰아주기
-      User winner =
+
+      // 3. 1등에게 나머지 팀원의 시드머니 몰아주기
+      // 3-1. 그룹 내 거래가 없었던 경우 몰아주기 X 다시 분배
+      if (!userGroupTransactionRepository.existsByUserGroup_Group_Id(group.getId())) {
+        for (UserGroupRankingDto rankingDto : rankingAndPnLInGroup) {
+          Long userId = rankingDto.getUserPnl().getUserId();
           userRepository
-              .findById(winnerId)
-              .orElseThrow(() -> new CustomException(UserErrorCode.USER_NOT_FOUND));
-      winner.addCashBalance(
-          group.getSeedMoney().multiply(BigDecimal.valueOf(group.getMemberCount())));
+              .findById(userId)
+              .ifPresent(
+                  (user) -> {
+                    user.addCashBalance(group.getSeedMoney());
+                  });
+        }
+        return;
+      }
+      // 3-2. 시드머니 몰아주기
+      userRepository
+          .findById(winnerId)
+          .ifPresent(
+              (winner) -> {
+                winner.addCashBalance(
+                    group.getSeedMoney().multiply(BigDecimal.valueOf(group.getMemberCount())));
+              });
     }
   }
 }
